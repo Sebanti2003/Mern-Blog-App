d1868c802d6cfb63929e3898a5c2f06e6233b0c7
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-01-17 16:21:11 +0530

    done

--- server/controllers/signup.controller.js
+++ server/controllers/signup.controller.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("../models/user.model.js");
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
 var salt = bcrypt.genSaltSync(10);
 exports.signup = async (req, res) => {
   try {
@@ -15,22 +15,23 @@ exports.signup = async (req, res) => {
       email === "" ||
       password === ""
     ) {
-      return res
-        .status(400)
-        .json({
-          Outcome: "Failed",
-          Status: 400,
-          message: "All credentials are required",
-        });
+      return res.status(400).json({
+        Outcome: "Failed",
+        Status: 400,
+        message: "All credentials are required",
+      });
     }
-    const hashedpassword=bcrypt.hashSync(password,salt);
-    const newuser = await User.create({username,email,password:hashedpassword});
-    if(newuser){
-        console.log("Done creating the user!!!!!");
-        return res.status(200).json({ Message: "Success", status: 200, newuser });
+    const hashedpassword = bcrypt.hashSync(password, salt);
+    const newuser = await User.create({
+      username,
+      email,
+      password: hashedpassword,
+    });
+    if (newuser) {
+      console.log("Done creating the user!!!!!");
+      return res.status(200).json({ Message: "Success", status: 200, newuser });
     }
-    
   } catch (error) {
-    res.status(404).json({status:404,error})
+    res.status(404).json({ status: 404, error });
   }
 };
