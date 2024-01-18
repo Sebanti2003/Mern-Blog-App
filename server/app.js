46b7093289447e0983a58e1da4c30b7474210a03
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-01-18 13:22:56 +0530

    add some comments to increase the readibility

--- server/app.js
+++ server/app.js
@@ -6,15 +6,21 @@ const Signup=require('./routes/signup.js');
 const Signinrouter=require('./routes/signin.js')
 const {hi}=require('./middlewares/hi.js');
 
+
+//important middlewares
 app.use(express.json());
 app.use(cors({
     origin:'http://localhost:5173/',
     methods:["GET","POST","PUT","PATCH","DELETE"]
 }));
+
+//test router
 app.use('/',TestRouter);
 
-
+//test middleware
 app.use('/api/users',hi)
+
+//auth routes
 app.use('/api/users',Signup);
 app.use('/api/users',Signinrouter);
 
